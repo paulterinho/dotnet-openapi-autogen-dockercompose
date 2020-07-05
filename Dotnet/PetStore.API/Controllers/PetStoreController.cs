f98ec8797b1c5b1c556438b9da72b10733488a22
paulito-bandito <paul.walter.software@gmail.com>
2020-07-05 12:33:59 -0400

    Hooked in automated build settings for NSwag

---
+++ Dotnet/PetStore.API/Controllers/PetStoreController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using petstore.swagger.io;
+
+namespace PetStore.API.Controllers
+{
+    /// <summary>
+    /// This class isn't customized at all. When you extend the base controller class, this is class you get.
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PetStoreController : PetStoreControllerBase
+    {
+
+        public override Task<Pet> AddPet([FromBody] NewPet body)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Task DeletePet(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Task<ICollection<Pet>> FindPets([FromQuery] IEnumerable<string> tags, [FromQuery] int? limit)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Task<Pet> Find_pet_by_id(long id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Dotnet/PetStore.API/Startup.cs
+++ Dotnet/PetStore.API/Startup.cs
@@ -26,6 +26,30 @@ namespace PetStore.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            // Register the Swagger services
+            services.AddSwaggerDocument(
+                config =>
+                {
+                    config.PostProcess = document =>
+                    {
+                        document.Info.Version = "v1";
+                        document.Info.Title = "ToDo API";
+                        document.Info.Description = "<h1>Welcome Medisked Colleagues!</h1> <p>If you are reading this it means you are interested in learning more about how OpenAPI can make our lives easier, and our projects clearer.</p> <p>Please click on the 'PetStore' header below to expand the web methods and examine our API (and test it without the need for Postman!).</p><p><b>Important Note: </b> <ul> <li> It is expected you will get a 'NotImplementedException' error.</li> <li>This project is only to show the scaffolding capabilites of using OpenAPI and Swagger.</li></ul></p><br/>";
+                        document.Info.Contact = new NSwag.OpenApiContact
+                        {
+                            Name = "Paul Walter",
+                            Email = "paul.walter.software@gmail.com",
+                            Url = "https://github.com/paulito-bandito"
+                        };
+                        document.Info.License = new NSwag.OpenApiLicense
+                        {
+                            Name = "Use under LICX",
+                            Url = "https://example.com/license"
+                        };
+                    };
+                }
+                );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -34,6 +58,10 @@ namespace PetStore.API
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+
+                // Register the Swagger generator and the Swagger UI middlewares
+                app.UseOpenApi();
+                app.UseSwaggerUi3();
             }
 
             app.UseHttpsRedirection();
